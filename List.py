e94e060624c420bf1ed49c0fb46867b7bfea8326
inshaAsif <inshaasif19@gmail.com>
2023-08-31 09:27:43 +0500

    list Items

---
+++ List.py
@@ -0,0 +1,44 @@
+class_1= ["abdullah", "noman", "raheel"]
+print(class_1)   #list
+
+class_2= ["insha", "Haris", "arqam", "Haris", "insha"]
+print(class_2)    #allow dulicates
+
+class_3= ["asad", "umer"]
+print(len(class_3))     #len
+
+class_4= ["abdullah", "noman", "raheel"]
+class_5= [1, 5, 9, 7, 8]
+class_6= [True, False, False]
+print(class_4)
+print(class_5)
+print(class_6)    #Data Types
+
+list_1= ["karachi", "lahore", "sialkot"]
+print(type(list_1))    #type ()
+list_2= ["insha", 44, True, 90, "saad"]
+print(type(list_2))    #type()
+
+class_7=list(("apple", "banana", "peach"))
+print(class_7)    #list constructor()
+
+class_8= ["abdullah", "noman", "raheel"]
+print(class_8[1])    #Access List
+
+class_9= ["ahmed", "huzaifa", "waqas", "zaid"]
+print(class_9[-2])   #negative Index
+
+class_10= ["kiwi", "apple", "banana", "coconut", "peach", "watermelon"]
+print(class_10[2:5])
+class_10= ["kiwi", "apple", "banana", "coconut", "peach", "watermelon"]
+print(class_10[:4])
+class_10= ["kiwi", "apple", "banana", "coconut", "peach", "watermelon"]
+print(class_10[2:])     #Range Indexes
+
+class_11= ["arsalan", "safdar", "shehroz", "bilal", "uzair", "rehan"]
+print(class_11[-5:-1])  #negative Indexes(range)
+
+class_12= ["raheel", "saad", "insha"]
+if "raheel" in class_12:
+    print("yes, 'raheel' is present")   #Check Item Exist
+
