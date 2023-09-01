acdad5601cd005ef271ae904284a87af3e5a6690
inshaAsif <inshaasif19@gmail.com>
2023-09-01 09:37:58 +0500

    list.py

--- List.py
+++ List.py
@@ -42,3 +42,47 @@ class_12= ["raheel", "saad", "insha"]
 if "raheel" in class_12:
     print("yes, 'raheel' is present")   #Check Item Exist
 
+class_13= ["Bat", "Hat", "Cat"]
+class_13[1]= "Rat"
+print(class_13)     #change Item value
+
+class_14= ["anas", "waqas", "aqil", "aqib", "rashid", "babar"]
+class_14[1:3]= "amin", "anees"
+print(class_14)    #change Range Items
+
+class_15= ["potato", "tomato", "ladyfinger"]
+class_15[1:3]= ["onion"]
+print(class_15)    #replacing with one value
+
+class_16= ["Karachi", "Lahore", "Sawaat", "sialkot"]
+class_16.insert(2, "Hyderabad")
+print(class_16)     #Insert Item
+
+class_17= ["pakistan", "sriLanka", "Bangladesh", "India"]
+class_17.append("Nepal")
+print(class_17)    #Add Items
+
+class_18= ["Java", "Python", "CSS"]
+class_18.insert(1, "Php")
+print(class_18)     #Insert Item
+
+class_19= ["apple", "coconut", "rasberry"]
+class_20= ["blueBerry", "orange", "Guava"]
+class_19.extend(class_20)
+print(class_19)     #extend List
+
+var= ["sun", "moon", "star"]
+var.remove("moon")
+print(var)   #Remove Items
+
+list_1= ["pakistan", "sriLanka", "Bangladesh", "India"]
+list_1.pop(2)
+print(list_1)     #Pop() remove specified Index
+
+list_2= ["apple", "coconut", "rasberry"]
+del list_2[1]
+print(list_2)      #del is also remove specified index
+
+list_3= ["sun", "moon", "star"]
+list_3.clear()
+print(list_3)        #clear List
